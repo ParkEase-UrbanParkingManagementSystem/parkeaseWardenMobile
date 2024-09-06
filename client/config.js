c7136cade42ebd57777a4b95f5a2d9834b64cacf
Nipul Yansith <nipulyansith26@gmail.com>
2024-09-06 23:18:29 +0530

    aaa

--- client/config.js
+++ client/config.js
@@ -1,4 +1,4 @@
 // warden/client/config.js
 // export const BASE_URL = 'http://192.168.238.186:5003';
 // export const BASE_URL = process.env.BASE_URL || 'http://192.168.238.186:5003';
-export const BASE_URL = process.env.BASE_URL || 'http://192.168.8.198:5000/wardenApp';
+export const BASE_URL = process.env.BASE_URL || 'http://192.168.244.147:5000/wardenApp';
